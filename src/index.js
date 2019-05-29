50259f99b63d43e287be61cf6242017038b59ff1
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-29 14:23:47 -0700

    logging actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import { createStore, applyMiddleware, compose } from 'redux';
+// import App from './components/App';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const reducer = (state, action) => {
+  const { type } = action;
+  switch(type) {
+    default:
+      return state;
+  }
+}
+
+const logger = store => next => action => {
+  console.log(action);
+}
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(
+      logger
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
+
+store.dispatch({
+  type: 'Hello'
+})
+
+// render(
+//   <App />,
+//   document.getElementById('root')
+// );
