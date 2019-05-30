615f2b1aa614aec412df25450b7e0798e4c13e9a
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-30 09:47:24 -0700

    more actions and reducer for loading

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -1,8 +1,15 @@
 import { getCharacters } from '../services/lastAirbenderApi';
 
 export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
+export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
+
 
 export const fetchCharacters = dispatch => {
+
+  dispatch({
+    type: FETCH_CHARACTERS_LOADING
+  });
+
   return getCharacters()
     .then(characters => {
       dispatch({
--- src/reducers/characterReducer.js
+++ src/reducers/characterReducer.js
@@ -1,3 +1,19 @@
-export default (state, action) => {
+import { FETCH_CHARACTERS, FETCH_CHARACTERS_LOADING } from '../actions/characterActions';
 
+const initialState = {
+  getCharactersLoading: false,
+  getCharactersError: null,
+  characters: []
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+  switch(type) {
+    case FETCH_CHARACTERS:
+      return { ...state, characters: payload, getCharactersLoading: false };
+    case FETCH_CHARACTERS_LOADING:
+      return { ...state, getCharactersLoading: true };
+    default:
+      return state;
+  }
 };
