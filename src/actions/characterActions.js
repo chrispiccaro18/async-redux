9e8c00bdfe05fac60a848b7d409340b121957d98
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-30 13:52:38 -0700

    that was easy

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -4,17 +4,7 @@ export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
 
 
-export const fetchCharacters = () => dispatch => {
-
-  dispatch({
-    type: FETCH_CHARACTERS_LOADING
-  });
-
-  return getCharacters()
-    .then(characters => {
-      dispatch({
-        type:FETCH_CHARACTERS,
-        payload: characters
-      });
-    });
-};
+export const fetchCharacters = () => ({
+  type:FETCH_CHARACTERS,
+  payload: getCharacters()
+});
