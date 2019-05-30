e6bec47b2555a5dad92cbb20178ee5bc1cb35b97
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-30 15:44:13 -0700

    character selector for character and character loading

--- src/selectors/characterSelectors.js
+++ src/selectors/characterSelectors.js
@@ -1,2 +1,4 @@
 export const selectCharacters = state => state.characters.characters;
 export const selectCharactersLoading = state => state.characters.loading;
+export const selectCharacter = state => state.character.character;
+export const selectCharacterLoading = state => state.character.loading;
