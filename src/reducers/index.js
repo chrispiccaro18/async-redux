868edce29ea351d9d55085947546aa64b2d51c2b
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-30 15:43:57 -0700

    added character reducer to combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import characters from './characterReducer';
+import character from './charReducer';
 
 export default combineReducers({
-  characters
+  characters,
+  character
 });
