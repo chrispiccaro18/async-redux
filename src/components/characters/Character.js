0c88fa81085208d306ab34efec6c516ba8ec90e7
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-30 09:23:49 -0700

    character component

---
+++ src/components/characters/Character.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Character({ character }) {
+  const { photoUrl, name } = character;
+  return (
+    <li>
+      <img src={photoUrl} alt={`Image of ${name}`} />
+      <h3>{name}</h3>
+    </li>
+  );
+}
+
+Character.propTypes = {
+  character: PropTypes.shape({
+    photoUrl: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  })
+};
+
+export default Character;
