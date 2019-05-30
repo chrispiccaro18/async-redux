4e3a4130b911d168c24ccd68e031085d5a3262e7
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-30 15:43:35 -0700

    added character action

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -1,8 +1,14 @@
-import { getCharacters } from '../services/lastAirbenderApi';
 import { createAction } from 'promise-middleware-redux';
+import { getCharacters, getCharacter } from '../services/lastAirbenderApi';
 
 export const [
   fetchCharacters,
   FETCH_CHARACTERS,
   FETCH_CHARACTERS_LOADING,
 ] = createAction('FETCH_CHARACTERS', getCharacters);
+
+export const [
+  fetchCharacter,
+  FETCH_CHARACTER,
+  FETCH_CHARACTER_LOADING,
+] = createAction('FETCH_CHARACTER', getCharacter);
