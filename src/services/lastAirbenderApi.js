7474eb79cc4390fb6ddcf9fd742cb36018b2378a
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-30 15:42:31 -0700

    created service to get one character

--- src/services/lastAirbenderApi.js
+++ src/services/lastAirbenderApi.js
@@ -8,3 +8,12 @@ export function getCharacters() {
       return body;
     });
 }
+
+export function getCharacter(id) {
+  return fetch(`${BASE_URL}/v1/characters/${id}`)
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, body]) => {
+      if(!ok) throw 'Could not fetch character';
+      return body;
+    });
+}
